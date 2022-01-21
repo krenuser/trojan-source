25717c2b9938e0ba3861ce5d95318fcd07315f9a
Ilja <32645945+krenuser@users.noreply.github.com>
2022-01-21 17:03:59 +0300

    Update commenting-out.cpp

--- C++/commenting-out.cpp
+++ C++/commenting-out.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-    bool isAdmin = false;
-    /*‮ } ⁦if (isAdmin)⁩ ⁦ begin admins only */
-        std::cout << "You are an admin.\n";
-    /* end admins only ‮ { ⁦*/
+    bool isDebug = false;
+    /*‮ } ⁦if (isDebug)⁩ ⁦ for debug mode only */
+        // ... open remote debugger
+    /* end debug mode only ‮ { ⁦*/
     return 0;
 }
